6f61cac52942fcf0a48f09218f2754c5489544ed
Daniel Moulton <dan.moulton@btinternet.com>
2022-10-30 14:41:17 +0000

    Card construtor + getter

--- Card.java
+++ Card.java
@@ -1,3 +1,13 @@
 public class Card {
-    
+    private final int CARD_VALUE;
+
+    public Card(int value) {
+        // Initialise the card's value
+        CARD_VALUE = value;
+    }
+
+    public int getCardValue() {
+        // Return the value of the card
+        return CARD_VALUE;
+    }
 }
