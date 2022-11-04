7dcfdda03243dca8b234bd6eb1725278f7fd26ba
Daniel Moulton <dan.moulton@btinternet.com>
2022-11-04 20:00:25 +0000

    More isValidPackFile tests

--- Test/TestCardGame.java
+++ Test/TestCardGame.java
@@ -19,8 +19,32 @@ public class TestCardGame {
     @Test
     public void testIsValidPackFileValidFile(){
         int numPlayers=5;
-        String filePath="Test/valid5PlayersPack.txt";
+        String filePath="/Users/danielmoulton/UniDocs/Year 2/Term 1/SoftwareDev/CA/threaded-card-sim/Test/valid5PlayersPack.txt";
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         assertTrue("Valid file not recognised as valid", CardGame.isValidPackFile(filePath, numPlayers));
+    }
+
+    @Test
+    public void testIsValidPackFileNonIntegerCard(){
+        int numPlayers=5;
+        String filePath="/Users/danielmoulton/UniDocs/Year 2/Term 1/SoftwareDev/CA/threaded-card-sim/Test/invalidString5PlayersPack.txt";
+        // Console message should read "ERROR: Pack file contains a non-integer"
+        assertFalse("Non-integer card not recognised as invalid", CardGame.isValidPackFile(filePath, numPlayers));
+    }
+
+    @Test
+    public void testIsValidPackFileInvalidFilePath(){
+        int numPlayers=5;
+        String filePath="thisIsNotAValidFilePath";
+        // Console message should read "ERROR: Pack file does not exist"
+        assertFalse("Invalid file path not recognised as invalid", CardGame.isValidPackFile(filePath, numPlayers));
+    }
+
+    @Test
+    public void testIsValidPackFileInvalidNumberOfCards(){
+        int numPlayers=3;
+        String filePath="/Users/danielmoulton/UniDocs/Year 2/Term 1/SoftwareDev/CA/threaded-card-sim/Test/valid5PlayersPack.txt";
+        // Console message should read "ERROR: There are no 24 cards in the pack file"        
+        assertFalse("Incorrect number of cards for number of players not recognised as invalid", CardGame.isValidPackFile(filePath, numPlayers));
     }
 }
