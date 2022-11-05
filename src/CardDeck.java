940f406b8eff0ca873227f5e6f1bbe826d077deb
jamespilcher <pilcherjames0@gmail.com>
2022-11-05 15:04:40 +0000

    player comments and queue addition

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,13 +1,18 @@
 package src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CardDeck{ //inherits WritesToFile
 
-        //contents ( card[])
+        // contents queue ( card[])
+        private Queue<Card> contents = new LinkedList<Card>();
         // folder to print to
 
         // generate 
 
-        // draw funciton
-        // insert (to bottom) function
+        // draw funciton  - remove from top, return value.
+        // insert (to bottom) function - remove from bottom, return value. 
 
         // Write to file, folder 
 
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,17 @@
 package src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Player {
 
     // current hand
-    private CardDeck hand;
-    // player number (determined by name)
-    private final int PLAYER_NUMBER;
-    // player name 
-    // folder to print to
+
+
+    private Card[] hand;
+
+    private Queue<Card> discardables = new LinkedList<Card>();
+
 
     // deck to draw from
     private CardDeck deckDrawnFrom;
@@ -14,11 +19,19 @@ public class Player {
     private CardDeck deckInsertedTo;
     // pass in deck into constructor
 
+
+    // player number (determined by name)
+    private final int PLAYER_NUMBER;
+    // player name 
+    // folder to print to
+
+
+
     public Player(int playerNumber, CardDeck deckDrawnFrom, CardDeck deckInsertedTo){
         // Initialise the player's number
         PLAYER_NUMBER = playerNumber;
         // Initialise the player's hand
-        hand = new CardDeck();
+        hand = new Card[4];
         // Initialise the deck to draw from
         this.deckDrawnFrom = deckDrawnFrom;
         // Initialise the deck to insert into
@@ -31,10 +44,24 @@ public class Player {
 
     // function: append string to their output file
 
+    // chooseDiscardables
+
+
+    // drawCard
+    //     alter drawn card variable, picking up from deck
+
+    // removeCard:
+    //   from discardables remove front of queue, remove from hand (empty slot 0), place into next deck,
+
+    // place drawncard:
+    //   add drawncard to empty slot in hand, if does not match desired number add to discardables
+
 
     // close file
 
     // function: writes deck contents files
 
+
+    // method that checks if their hand wins
 
 }
