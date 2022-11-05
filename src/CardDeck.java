5453732cc71ae3e16e72e175284edd538fb7a2f3
jamespilcher <pilcherjames0@gmail.com>
2022-11-05 13:41:43 +0000

    card outline

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,4 +1,4 @@
 package src;
 public class CardDeck{
-    
+        //contents ( card[])
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,7 @@
 package src;
 public class Player {
-    
+    //current hand
+    // prefered hand
+    // player name 
+    // prefered player number (determined by name)
 }
