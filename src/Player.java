937929a18c7156a4f036e0206477dd03135854cd
jamespilcher <pilcherjames0@gmail.com>
2022-11-05 13:49:07 +0000

    player details

--- src/Player.java
+++ src/Player.java
@@ -3,4 +3,14 @@ public class Player {
     // current hand
     // player name 
     // prefered player number (determined by name)
+
+
+
+    // in constructor, create a file for them. (using their name)
+
+
+    // function: append string to their output file
+
+
+    // close file
 }
