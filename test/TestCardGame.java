d2b2e970ad2a93645b309f7f09d35591913fae61
Daniel Moulton <dan.moulton@btinternet.com>
2022-11-18 13:11:43 +0000

    TestCardGame update

--- test/TestCardGame.java
+++ test/TestCardGame.java
@@ -3,7 +3,7 @@ package test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
+import java.util.concurrent.BrokenBarrierException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,19 +17,22 @@ import src.CardGame;
  * @author James Pilcher
  */
 public class TestCardGame {
-  public CardGame game;
+  public static CardGame game;
 
   @Before
-  public void setUp() {
-    Card[] cards = CardGame.readInPack("valid5PlayersPack.txt", 5);
-    game = new CardGame(5);
+  public void setUp() throws InterruptedException, BrokenBarrierException {
+    // game = new CardGame(5);
+    // game.startPlayerThreads();
+    // game.releaseBarrier();
+    Card[] cards = CardGame.getInputPack(5);
+    game = new CardGame(5, cards);
+
   }
 
   @Test
   public void testIsValidPackFileValidFile() {
     int numPlayers = 5;
-    String filePath = "Test/valid5PlayersPack.txt";
-    System.out.println(new File(".").getAbsolutePath());
+    String filePath = "test/valid5PlayersPack.txt";
     assertTrue("Valid file not recognised as valid", CardGame.isValidPackFile(
         filePath, numPlayers));
   }
@@ -37,7 +40,7 @@ public class TestCardGame {
   @Test
   public void testIsValidPackFileNonIntegerCard() {
     int numPlayers = 5;
-    String filePath = "Test/invalidString5PlayersPack.txt";
+    String filePath = "test/invalidString5PlayersPack.txt";
     // Console message should read "ERROR: Pack file contains a non-integer"
     assertFalse("Non-integer card not recognised as invalid", CardGame.isValidPackFile(
         filePath, numPlayers));
@@ -55,7 +58,7 @@ public class TestCardGame {
   @Test
   public void testIsValidPackFileInvalidNumberOfCards() {
     int numPlayers = 3;
-    String filePath = "Test/valid5PlayersPack.txt";
+    String filePath = "test/valid5PlayersPack.txt";
     // Console message should read "ERROR: There are not 24 cards in the pack file"
     assertFalse("Incorrect number of cards for number of players not recognised as invalid",
         CardGame.isValidPackFile(filePath, numPlayers));
@@ -64,7 +67,7 @@ public class TestCardGame {
   @Test
   public void testIsValidPackFileNegativeIntegerCard() {
     int numPlayers = 5;
-    String filePath = "Test/invalidNegative5PlayersPack.txt";
+    String filePath = "test/invalidNegative5PlayersPack.txt";
     // Console message should read "ERROR: Pack file contains a non-positive
     // integer"
     assertFalse("Negative integer card not recognised as invalid", CardGame.isValidPackFile(
